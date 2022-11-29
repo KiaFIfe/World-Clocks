566e6ca8178c160f3cfeb759d42fe80e7c5d2fa0
KiaFIfe <likiafife1@googlemail.com>
2022-11-29 19:09:12 +0000

    added id so that i cand start to get rid of the fake data

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 function timeZone(event) {
   if (event.target.value.length > 0) {
-    if (event.target.value === "sydney") {
-      alert(`It is ${moment()
-        .tz("Australia/Sydney")
-        .format("dddd, MMMM, D, H:m A")}
- in Sydney`);
+    if (event.target.value === "los-angeles") {
+      let timeL = document.querySelector("#timeLa");
+      timeL.innerHTML = moment().tz("America/Los_Angeles").format(" H:m:s ");
     }
-    if (event.target.value === "paris") {
-      alert(`It is ${moment().tz("Europe/Paris").format("dddd, MMMM, D, H:m A")}
- in Paris`);
-    }
-    if (event.target.value === "tokyo")
-      alert(`It is ${moment().tz("Asia/Tokyo").format("dddd, MMMM, D, H:m A")}
- in Tokyo`);
   }
 }
 
-let sydneySelect = document.querySelector("#dropBox");
-sydneySelect.addEventListener("change", timeZone);
+let tzSelect = document.querySelector("#dropBox");
+tzSelect.addEventListener("change", timeZone);
+
+setInterval("timeZone()", 1000);
