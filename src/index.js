3381ca33bc7d92920b3a050fc424a03cdf4b8919
KiaFIfe <likiafife1@googlemail.com>
2022-11-30 14:44:38 +0000

    Created a function to show the selected city js

--- src/index.js
+++ src/index.js
@@ -1,41 +1,62 @@
-let dateS = document.querySelector("#dateSydney");
-let timeS = document.querySelector("#timeSydney");
-let amPmS = document.querySelector("#amPmS");
-dateS.innerHTML = moment().tz("Australia/Sydney").format("MMMM Do YYYY");
-timeS.innerHTML = `${moment()
-  .tz("Australia/Sydney")
-  .format(" HH:mm:SS")} <small class="am-pm">${moment()
-  .tz("Australia/Sydney")
-  .format("A")}`;
+function timeZone() {
+  let dateS = document.querySelector("#dateSydney");
+  let timeS = document.querySelector("#timeSydney");
+  dateS.innerHTML = moment().tz("Australia/Sydney").format("MMMM Do YYYY");
+  timeS.innerHTML = `${moment()
+    .tz("Australia/Sydney")
+    .format(" HH:mm:ss")} <small class="am-pm">${moment()
+    .tz("Australia/Sydney")
+    .format("A")}`;
 
-// let tzSelect = document.querySelector("#dropBox");
-// tzSelect.addEventListener("change", timeZone);
-let dateP = document.querySelector("#dateParis");
-let timeP = document.querySelector("#timeParis");
-let amPmP = document.querySelector("#amPmP");
-dateP.innerHTML = moment().tz("Europe/Paris").format("MMMM Do YYYY");
-timeP.innerHTML = `${moment()
-  .tz("Europe/Paris")
-  .format("HH:MM:SS")} <small class="am-pm">${moment()
-  .tz("Europe/Paris")
-  .format("A")}`;
+  let dateP = document.querySelector("#dateParis");
+  let timeP = document.querySelector("#timeParis");
+  dateP.innerHTML = moment().tz("Europe/Paris").format("MMMM Do YYYY");
+  timeP.innerHTML = `${moment()
+    .tz("Europe/Paris")
+    .format("HH:mm:ss")} <small class="am-pm">${moment()
+    .tz("Europe/Paris")
+    .format("A")}`;
 
-let dateT = document.querySelector("#dateTokyo");
-let timeT = document.querySelector("#timeTokyo");
-let amPmT = document.querySelector("#amPmT");
-dateT.innerHTML = moment().tz("Asia/Tokyo").format("MMMM Do YYYY");
-timeT.innerHTML = `${moment()
-  .tz("Asia/Tokyo")
-  .format("HH:MM:SS")} <small class="am-pm">${moment()
-  .tz("Asia/Tokyo")
-  .format("A")}`;
+  let dateT = document.querySelector("#dateTokyo");
+  let timeT = document.querySelector("#timeTokyo");
+  dateT.innerHTML = moment().tz("Asia/Tokyo").format("MMMM Do YYYY");
+  timeT.innerHTML = `${moment()
+    .tz("Asia/Tokyo")
+    .format("HH:MM:ss")} <small class="am-pm">${moment()
+    .tz("Asia/Tokyo")
+    .format("A")}`;
 
-let dateLa = document.querySelector("#dateLa");
-let timeLa = document.querySelector("#timeLa");
-let amPmL = document.querySelector("#amPmL");
-dateLa.innerHTML = moment().tz("America/Los_Angeles").format("MMMM Do YYYY");
-timeLa.innerHTML = `${moment()
-  .tz("America/Los_Angeles")
-  .format("HH:MM:SS")} <small class="am-pm">${moment()
-  .tz("America/Los_Angeles")
-  .format("A")}`;
+  let dateLa = document.querySelector("#dateLa");
+  let timeLa = document.querySelector("#timeLa");
+  dateLa.innerHTML = moment().tz("America/Los_Angeles").format("MMMM Do YYYY");
+  timeLa.innerHTML = `${moment()
+    .tz("America/Los_Angeles")
+    .format("HH:mm:ss")} <small class="am-pm">${moment()
+    .tz("America/Los_Angeles")
+    .format("A")}`;
+}
+
+function locationTimeZone(event) {
+  let selectTimeZoneElement = document.querySelector("#box");
+  let changeTimeZone = event.target.value;
+  let changeName = changeTimeZone.replace("_", " ").split("/")[1];
+  let locationdata = moment().tz(changeTimeZone);
+  selectTimeZoneElement.innerHTML = `
+  <div class="city-box">
+  <div class="left">
+  <h2 class="name">${changeName}</h2>
+  <div class="month">${locationdata.format("MMM Do YYYY")}</div>
+  </div>
+  <div class="time"> ${locationdata.format("HH:MM:ss")}
+  <small class="am-pm">${locationdata.format("A")}</small>
+  </div>
+  </div>
+  </div>`;
+}
+
+let tzSelect = document.querySelector("#dropBox");
+tzSelect.addEventListener("change", locationTimeZone);
+let box = document.querySelector("#box");
+
+setInterval(timeZone, 1000);
+setInterval(locationTimeZone, 1000)
